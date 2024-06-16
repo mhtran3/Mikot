e0f304e4660df6e0df66082b1f3122f84e4e1b12
mhtran3 <mhtran@princeton.edu>
2024-06-16 15:53:42 -0500

    forgot to add script for header

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-// add footer for each page
 document.addEventListener("DOMContentLoaded", function() {
+    // Load header
+    fetch("header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header-placeholder").innerHTML = data;
+        })
+        .catch(error => console.error('Error loading header:', error));
+    
+    // Load footer
     fetch("footer.html")
         .then(response => response.text())
         .then(data => {
